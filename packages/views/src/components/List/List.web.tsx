03c54ff2937fad0961b6a9459e99426438e71ecc
YajanaRao <yajananrao@gmail.com>
2020-06-25 21:43:12 +0530

    added hover effect to fab and list

---
+++ packages/views/src/components/List/List.web.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import {
+    StyleProp,
+    Text,
+    View,
+    ViewStyle,
+} from "react-native";
+import { useTheme } from "../../theme";
+import { Theme } from "../../types";
+import { Icon } from "../Icons";
+import { Hoverable } from "../Hoverable/Hoverable"
+
+const Color = require("color");
+
+
+
+export interface Props {
+    title?: string;
+    description?: string;
+    icon?: string;
+    onPress?: () => void;
+    style?: StyleProp<ViewStyle>;
+    active?: boolean;
+}
+
+const LIST_CONTAINER: ViewStyle = {
+    padding: 12,
+    marginVertical: 2,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flex: 1,
+};
+
+const ICON_CONTAINER: ViewStyle = {
+    maxWidth: 40,
+    marginRight: 16,
+    justifyContent: "center",
+    alignItems: "center",
+};
+
+const TEXT_CONTAINER: ViewStyle = {
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+};
+
+export const List = ({
+    title,
+    description,
+    icon,
+    onPress,
+    style,
+    active = false,
+}: Props) => {
+    const theme: Theme = useTheme();
+    const {
+        border,
+        colors: { surface, text, primary, accent },
+        roundness,
+    } = theme;
+
+    const getActiveTextColor = () => {
+        let textColor = "black";
+        let color = Color(primary);
+        if (color.isDark()) {
+            textColor = "white";
+        }
+        return textColor;
+    };
+
+    let backgroundColor = active ? primary : surface;
+    let textColor = active ? getActiveTextColor() : text;
+    const titleColor = Color(textColor).alpha(0.87).rgb().string();
+    const descriptionColor = Color(textColor).alpha(0.54).rgb().string();
+
+    const color = Color(textColor);
+    let borderColor = color.fade(0.75).hex();
+
+    return (
+        <Hoverable
+            style={[
+                LIST_CONTAINER,
+                {
+                    backgroundColor,
+                    borderRadius: roundness,
+                    borderWidth: border,
+                    borderColor,
+                },
+                style
+            ]}
+            onHover={active ? { backgroundColor: primary } : { backgroundColor: accent }}
+            onPress={onPress}
+        >
+
+            {icon ? (
+                <View style={ICON_CONTAINER}>
+                    <Icon name={icon} color={descriptionColor} />
+                </View>
+            ) : (
+                    false
+                )}
+            <View style={TEXT_CONTAINER}>
+                <Text style={{ color: titleColor, fontSize: 14 }}>{title}</Text>
+                {description ? (
+                    <Text style={{ fontSize: 12, color: descriptionColor, marginTop: 2 }}>
+                        {description}
+                    </Text>
+                ) : (
+                        false
+                    )}
+            </View>
+        </Hoverable>
+    );
+};
